914a5f78d6f17866b106de6cede9938ebf17ce00
Olessya-Kadzhu <165058187+Olessya-Kadzhu@users.noreply.github.com>
2024-07-15 21:24:50 +0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,55 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+<div class="container_category"> 
+<h1 @click="toggleCategories">Категории</h1>
+</div>
+<div class="categories_items">
+<ul v-if="isOpen">
+      <li v-for="category in categories" :key="category.id">
+        <CategoryItem :category="category" />
+      </li>
+    </ul>
+</div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import CategoryItem from "./components/CategoryItem.vue"; 
+import categoryData from "./category.json"; 
+
+export default {
+  name: "App",
+  components: {
+    CategoryItem,
+  },
+  data() {
+    return {
+      isOpen: false,
+      categories: categoryData, 
+    };
+  },
+  methods: {
+    toggleCategories() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1{
+    cursor: pointer;
 }
 
-nav {
-  padding: 30px;
+#app{
+  display: flex;
+  flex-direction: row;
+  width: 50%;
+  font-size:10px;
+}
+li{
+ list-style: none;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <li>
+    <div class="category-header" @click="toggle">
+      <img
+        v-if="category.fileUrlSmall"
+        :src="category.fileUrlSmall"
+        alt="Category Image"
+        class="category-image"
+      />
+      <h2>{{ category.name }}</h2>
+    </div>
+    <ul v-if="isOpen && category.children && category.children.length">
+      <li v-for="child in category.children" :key="child.id">
+        <CategoryItem :category="child" />
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'CategoryItem',
+  props: {
+    category: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen;
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  font-size: 50px;
+}
+
+.category-header {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  background-color: #f0f0f0;
+  margin-bottom: 5px;
+  font-size:10px;
+}
+
+.category-header h2 {
+  margin: 0;
+  flex: 1; 
+}
+
+.category-image {
+  width: 20px;
+  height: 20px;
+  object-fit: contain;
+  margin-right: 10px; 
+}
+</style>
